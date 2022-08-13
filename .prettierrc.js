cd3ada9a73a25127abb0d9a4dde201e6864d705e
Daniel Brož <danobroz66@gmail.com>
2022-08-13 17:04:27 +0200

    add prettiers rules

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    trailingComma: 'es5',
+    tabWidth: 4,
+    semi: false,
+    singleQuote: true,
+    printWidth: 130,
+    endOfLine: 'lf',
+    jsxSingleQuote: true,
+    bracketSpacing: true,
+    "singleAttributePerLine": true,
+}
