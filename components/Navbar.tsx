f6310f9ceefb405260e11465e589a85b33dc7686
Daniel Brož <danobroz66@gmail.com>
2022-08-14 19:34:49 +0200

    add navbar bottom padding

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,7 +2,7 @@ import { NavigationItemsContainer } from '../containers'
 
 export const Navbar = () => {
     return (
-        <div className='container flex items-center justify-between pt-11'>
+        <div className='container flex items-center justify-between pt-11 pb-11'>
             <NavigationItemsContainer />
         </div>
     )
