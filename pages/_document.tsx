f240f23edf188f849a6fa69b77a05810bf91e0aa
Daniel Brož <danobroz66@gmail.com>
2022-08-15 20:08:02 +0200

    add font weights

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -16,11 +16,11 @@ export default function Document() {
                     href='/favicon.png'
                 />
                 <link
-                    href='https://fonts.googleapis.com/css2?family=Fraunces'
+                    href='https://fonts.googleapis.com/css2?family=Fraunces:wght@100;200;300;400;500;600;700;800;900&display=swap'
                     rel='stylesheet'
                 />
                 <link
-                    href='https://fonts.googleapis.com/css2?family=Barlow'
+                    href='https://fonts.googleapis.com/css2?family=Barlow:wght@100;200;300;400;500;600;700;800;900&display=swap'
                     rel='stylesheet'
                 />
             </Head>
