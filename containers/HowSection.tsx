bc17a7afa91fa49e8d38e68f50e55130b961288c
Daniel Brož <danobroz66@gmail.com>
2022-08-22 16:59:51 +0200

    add how section

--- containers/HowSection.tsx
+++ containers/HowSection.tsx
@@ -1,26 +1,33 @@
+import { Button, HowStep } from '../components'
+
 export const HowSection = () => {
     return (
-        <section className='px-[85px]'>
+        <section className='mb-[200px] px-[85px]'>
             <span className='mb-[80px] inline-block font-fraunces text-2xl font-bold text-Grey'>How it works</span>
             <div className='bubble-container relative mb-[67px] flex max-w-[791px] items-center justify-between'>
                 <span className='bubble'></span>
                 <span className='bubble'></span>
                 <span className='bubble'></span>
             </div>
-            <div>
-                <div className='max-w-[285px]'>
-                    <span className='inline-block pb-[38px] font-fraunces text-[72px] font-bold leading-none text-paleOrange'>
-                        01
-                    </span>
-                    <span className='inline-block pr-[30px] pb-[42px] font-fraunces text-[32px] font-bold leading-[36px] text-darkGreyBlue'>
-                        Pick your coffee
-                    </span>
-                    <p className='font-barlow leading-[1.625rem]'>
-                        Select from our evolving range of artisan coffees. Our beans are ethically sourced and we pay fair prices
-                        for them. There are new coffees in all profiles every month for you to try out.
-                    </p>
-                </div>
+            <div className='mb-[64px] grid grid-cols-3'>
+                <HowStep
+                    stepNumber={1}
+                    headline='Pick your coffee'
+                    content='Select from our evolving range of artisan coffees. Our beans are ethically sourced and we pay fair prices
+                        for them. There are new coffees in all profiles every month for you to try out.'
+                />
+                <HowStep
+                    stepNumber={2}
+                    headline='Choose the frequency'
+                    content='Customize your order frequency, quantity, even your roast style and grind type. Pause, skip or cancel your subscription with no commitment through our online portal.'
+                />
+                <HowStep
+                    stepNumber={3}
+                    headline='Receive and enjoy!'
+                    content='We ship your package within 48 hours, freshly roasted. Sit back and enjoy award-winning world-class coffees curated to provide a distinct tasting experience.'
+                />
             </div>
+            <Button>Create your plan</Button>
         </section>
     )
 }
