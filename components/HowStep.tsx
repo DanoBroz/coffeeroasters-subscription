a442145696f568132dc93193c2598bb25ee49eba
Daniel Brož <danobroz66@gmail.com>
2022-08-22 20:34:19 +0200

    set how section for dark background

--- components/HowStep.tsx
+++ components/HowStep.tsx
@@ -2,16 +2,21 @@ interface HowStepProps {
     stepNumber: number
     headline: string
     content: string
+    isDark?: boolean
 }
 
 export const HowStep = (props: HowStepProps) => {
-    const { stepNumber, headline, content } = props
+    const { stepNumber, headline, content, isDark = false } = props
     return (
         <div className='max-w-[285px]'>
             <span className='inline-block pb-[38px] font-fraunces text-[72px] font-bold leading-none text-paleOrange'>
                 0{stepNumber}
             </span>
-            <span className='inline-block pr-[30px] pb-[42px] font-fraunces text-[32px] font-bold leading-[36px] text-darkGreyBlue'>
+            <span
+                className={`inline-block pr-[30px] pb-[42px] font-fraunces text-[32px] font-bold leading-[36px] ${
+                    isDark ? 'text-lightCream' : 'text-darkGreyBlue'
+                }`}
+            >
                 {headline}
             </span>
             <p className='font-barlow leading-[1.625rem]'>{content}</p>
--- containers/HowSection.tsx
+++ containers/HowSection.tsx
@@ -1,33 +1,51 @@
+import { HTMLAttributes } from 'react'
 import { Button, HowStep } from '../components'
+import classnames from 'classnames'
 
-export const HowSection = () => {
+interface HowSectionConfig {
+    isDark?: boolean
+}
+
+type HowSectionProps = HTMLAttributes<HTMLElement> & HowSectionConfig
+
+export const HowSection = ({ className, isDark = false }: HowSectionProps) => {
     return (
-        <section className='mb-[200px] px-[85px]'>
-            <span className='mb-[80px] inline-block font-fraunces text-2xl font-bold text-Grey'>How it works</span>
+        <section
+            className={classnames(
+                {
+                    'rounded-[10px] bg-darkGreyBlue text-lightCream': isDark,
+                },
+                className
+            )}
+        >
+            {!isDark && <span className='mb-[80px] inline-block font-fraunces text-2xl font-bold text-Grey'>How it works</span>}
             <div className='bubble-container relative mb-[67px] flex max-w-[791px] items-center justify-between'>
-                <span className='bubble'></span>
-                <span className='bubble'></span>
-                <span className='bubble'></span>
+                <span className={`bubble ${isDark ? 'bg-darkGreyBlue' : ''}`}></span>
+                <span className={`bubble ${isDark ? 'bg-darkGreyBlue' : ''}`}></span>
+                <span className={`bubble ${isDark ? 'bg-darkGreyBlue' : ''}`}></span>
             </div>
-            <div className='mb-[64px] grid grid-cols-3'>
+            <div className={`${isDark ? '' : 'mb-[64px]'} grid grid-cols-3`}>
                 <HowStep
+                    isDark={isDark}
                     stepNumber={1}
                     headline='Pick your coffee'
                     content='Select from our evolving range of artisan coffees. Our beans are ethically sourced and we pay fair prices
                         for them. There are new coffees in all profiles every month for you to try out.'
                 />
                 <HowStep
+                    isDark={isDark}
                     stepNumber={2}
                     headline='Choose the frequency'
                     content='Customize your order frequency, quantity, even your roast style and grind type. Pause, skip or cancel your subscription with no commitment through our online portal.'
                 />
                 <HowStep
+                    isDark={isDark}
                     stepNumber={3}
                     headline='Receive and enjoy!'
                     content='We ship your package within 48 hours, freshly roasted. Sit back and enjoy award-winning world-class coffees curated to provide a distinct tasting experience.'
                 />
             </div>
-            <Button>Create your plan</Button>
+            {!isDark && <Button>Create your plan</Button>}
         </section>
     )
 }
