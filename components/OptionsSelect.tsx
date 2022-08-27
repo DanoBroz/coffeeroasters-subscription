dc6953988f07d6ee6e268312e806250b22c48fd5
Daniel Brož <danobroz66@gmail.com>
2022-08-27 07:12:30 +0200

    add accordion button to options select

--- components/OptionsSelect.tsx
+++ components/OptionsSelect.tsx
@@ -22,20 +22,29 @@ export function OptionsSelect({ select, setSelect, data }: OptionsSelectProps) {
     }
 
     return (
-        <div className='grid grid-cols-3 gap-x-[23px]'>
-            {data.map((item, index) => (
-                <button
-                    key={`${item.heading}-${index}`}
-                    className={classnames('flex h-[250px] flex-col gap-6 rounded-lg px-7 py-8 text-left transition-colors', {
-                        'bg-[#F4F1EB] text-darkGreyBlue hover:bg-paleOrange': select !== item.heading,
-                        'bg-darkCyan text-lightCream': select === item.heading,
-                    })}
-                    onClick={(e) => handleOptionChange(e, item.heading, setSelect)}
-                >
-                    <span className='block font-fraunces text-2xl font-bold'>{item.heading}</span>
-                    <span className='font-ligth block font-barlow leading-[26px]'>{item.text}</span>
-                </button>
-            ))}
+        <div>
+            <button className='flex w-full items-center justify-between font-fraunces text-[2.5rem] font-bold leading-[3rem] text-Grey transition-colors hover:text-darkGreyBlue'>
+                <span className='inline-block'>How do you drink your coffee?</span>
+                <span
+                    style={{ backgroundImage: `url('/assets/plan/desktop/icon-arrow.svg')` }}
+                    className='inline-block h-[11.92px] w-[18.19px] bg-cover bg-center bg-no-repeat'
+                ></span>
+            </button>
+            <div className='grid grid-cols-3 gap-x-[23px]'>
+                {data.map((item, index) => (
+                    <button
+                        key={`${item.heading}-${index}`}
+                        className={classnames('flex h-[250px] flex-col gap-6 rounded-lg px-7 py-8 text-left transition-colors', {
+                            'bg-[#F4F1EB] text-darkGreyBlue hover:bg-paleOrange': select !== item.heading,
+                            'bg-darkCyan text-lightCream': select === item.heading,
+                        })}
+                        onClick={(e) => handleOptionChange(e, item.heading, setSelect)}
+                    >
+                        <span className='block font-fraunces text-2xl font-bold'>{item.heading}</span>
+                        <span className='font-ligth block font-barlow leading-[26px]'>{item.text}</span>
+                    </button>
+                ))}
+            </div>
         </div>
     )
 }
